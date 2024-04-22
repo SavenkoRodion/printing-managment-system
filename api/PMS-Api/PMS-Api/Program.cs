2e3720416991cc15faa2fa169ac04f2087d49b79
RodionSavenko <savenko662@gmail.com>
2024-04-23 00:48:17 +0200

    Init

---
+++ api/PMS-Api/PMS-Api/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using PMS_Api.Tmp;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+var jwtOptions = builder.Configuration
+    .GetSection("JwtOptions")
+    .Get<JwtOptions>();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(jwtOptions);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opts =>
+    {
+        //convert the string signing key to byte array
+        byte[] signingKeyBytes = Encoding.UTF8
+            .GetBytes(jwtOptions.SigningKey);
+
+        opts.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtOptions.Issuer,
+            ValidAudience = jwtOptions.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
+        };
+    });
+
+// 👇 Configuring the Authorization Service
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// 👇 This add the Authentication Middleware
+app.UseAuthentication();
+// 👇 This add the Authorization Middleware
+app.UseAuthorization();
+
+// 👇 The routes / and /public allow anonymous requests
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/public", () => "Public Hello World!")
+    .AllowAnonymous();
+
+// 👇 The routes /private require authorized request
+app.MapGet("/private", () => "Private Hello World!")
+    .RequireAuthorization();
+
+// 👇 handles the request token endpoint
+app.MapPost("/tokens/connect", (HttpContext ctx, JwtOptions jwtOptions)
+    => TokenEndpoint.Connect(ctx, jwtOptions));
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
