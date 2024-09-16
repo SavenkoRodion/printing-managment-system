395236ec9fb3fcd0c229084b5cb3a6d055703087
RodionSavenko <savenko662@gmail.com>
2024-09-16 21:11:01 +0200

    Table primitive styling and create modal

--- ui/pms-ui/src/components/products/ProductList.tsx
+++ ui/pms-ui/src/components/products/ProductList.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import { Box, Button } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Product from "../../model/Product";
 
 type ProductListProps = {
@@ -7,9 +7,56 @@ type ProductListProps = {
 };
 
 const ProductList = ({ rows }: ProductListProps) => {
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "Id",
+      align: "center",
+      headerAlign: "center",
+      flex: 0.15,
+    },
+    {
+      field: "name",
+      headerName: "Nazwa produktu",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+    },
+    {
+      field: "dateOfCreation",
+      headerName: "Data stworzenia",
+      align: "center",
+      headerAlign: "center",
+      flex: 1,
+      disableReorder: true,
+    },
+    {
+      field: "buttons",
+      headerName: "Akcje",
+      flex: 0.3,
+      align: "center",
+      headerAlign: "center",
+      disableColumnMenu: true,
+      disableExport: true,
+      disableReorder: true,
+      renderCell: () => (
+        <Box>
+          <Button>Edytuj</Button>
+          <Button>Usun</Button>
+        </Box>
+      ),
+    },
+  ];
   return (
     <Box>
-      <DataGrid rows={rows} columns={[]}></DataGrid>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        rowSelection={false}
+        checkboxSelection={false}
+        disableColumnMenu
+        pageSizeOptions={[]}
+      ></DataGrid>
     </Box>
   );
 };
--- ui/pms-ui/src/components/products/Products.tsx
+++ ui/pms-ui/src/components/products/Products.tsx
@@ -1,11 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import ProductList from "./ProductList";
 import getAxiosClient from "../../utility/getAxiosClient";
 import { useEffect, useState } from "react";
 import Product from "../../model/Product";
+import styles from "./Products.style";
+import CreateProductModal from "./CreateProductDialog";
 
 const ProductPage = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
 
   useEffect(() => {
     const client = getAxiosClient();
@@ -13,11 +16,20 @@ const ProductPage = () => {
   }, []);
 
   return (
-    <Box>
-      Product Page
-      <>
-        <ProductList rows={products} />
-      </>
+    <Box width={"100%"} padding={"10px 50px 0 50px"}>
+      <Typography>Product Page</Typography>
+      <ProductList rows={products} />
+      <Button
+        variant={"contained"}
+        sx={styles.createButton}
+        onClick={() => setIsCreateModalOpen(true)}
+      >
+        Dodaj nowy produkt
+      </Button>
+      <CreateProductModal
+        isOpen={isCreateModalOpen}
+        handleClose={() => setIsCreateModalOpen(false)}
+      ></CreateProductModal>
     </Box>
   );
 };
