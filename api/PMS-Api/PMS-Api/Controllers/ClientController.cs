ba492b5231c21fc16e418655e059f7015f85f0ea
Piotr Marczak <141434625+icewuuu@users.noreply.github.com>
2024-11-08 13:59:44 +0100

    Basic backend structure for Projects, Templates, Client (#25)
    * Naprawa wielkosci po usunieciu kolumny "Aktywny upload"
    
    * Simple backend for Project/Pattern + Client(just for selector purposes)
    
    * Naming convention change + migration
    
    * Client ID int to Guid change + migration fix

---
+++ api/PMS-Api/PMS-Api/Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PMS_Api.Model.DbModel;
+using PMS_Api.Model.Requests;
+using PMS_Api.Repository;
+
+namespace PMS_Api.Controllers;
+
+[Authorize]
+[Route("api/client")]
+[ApiController]
+public class ClientController(IClientRepository repository) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IReadOnlyList<Client>> GetAsync(CancellationToken cancellationToken)
+    {
+        return await repository.GetAllAsync(cancellationToken);
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Client>> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        var client = await repository.GetByIdAsync(id, cancellationToken);
+        if (client == null)
+        {
+            return NotFound();
+        }
+        return Ok();
+    }
+}
---
+++ api/PMS-Api/PMS-Api/Repository/ClientRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMS_Api.Model.DbModel;
+
+namespace PMS_Api.Repository;
+
+public interface IClientRepository
+{
+    Task<IReadOnlyList<Client>> GetAllAsync(CancellationToken cancellationToken);
+    Task<Client?> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+}
+
+public class ClientRepository(PmsContext context) : IClientRepository
+{
+    public async Task<IReadOnlyList<Client>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await context.Clients.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+  public async Task<Client?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        var client = await context.Clients.FindAsync(id);
+        return client ?? default;
+    }
+}
