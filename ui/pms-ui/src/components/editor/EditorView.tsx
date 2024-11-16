a6047c8d459a5d4a9720607983d2f7f69d65577e
Adrian Smoła <Adrian.Smola@microsoft.wsei.edu.pl>
2024-11-16 20:53:54 +0100

    zoom-handle

--- ui/pms-ui/src/components/editor/EditorView.tsx
+++ ui/pms-ui/src/components/editor/EditorView.tsx
@@ -14,6 +14,9 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import AddIcon from "@mui/icons-material/Add";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { zoomPlugin } from "@react-pdf-viewer/zoom";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import "@react-pdf-viewer/zoom/lib/styles/index.css";
 
 interface EditorViewProps {}
 
@@ -22,6 +25,8 @@ const EditorView: React.FC<EditorViewProps> = () => {
   const [name, setName] = useState<string>("");
   const [client, setClient] = useState<string>("");
   const [file, setFile] = useState<File | null>(null); //przechowujemy file po wyborze PDF lub null przed wyborem
+  const zoomPluginInstance = zoomPlugin();
+  const { ZoomIn, ZoomOut, CurrentScale } = zoomPluginInstance;
 
   const rows = [
     { id: 1, type: "text", zIndex: 1, value: "Cennik usług" },
@@ -37,11 +42,11 @@ const EditorView: React.FC<EditorViewProps> = () => {
     },
     { field: "type", headerName: "Typ", width: 120 },
     { field: "zIndex", headerName: "Z-Index", width: 90 },
-    { field: "value", headerName: "Wartość", width: 180 },
+    { field: "value", headerName: "Wartość", width: 140 },
     {
       field: "actions",
       headerName: "",
-      flex: 1,
+      flex: 2,
       renderCell: () => (
         <Box>
           <IconButton aria-label="edit">
@@ -153,7 +158,7 @@ const EditorView: React.FC<EditorViewProps> = () => {
         width="50%"
         display="flex"
         flexDirection="column"
-        gap={1}
+        gap={2}
         alignItems="center"
       >
         <Button
@@ -175,15 +180,23 @@ const EditorView: React.FC<EditorViewProps> = () => {
             style={{
               height: "955px",
               width: "100%",
-              overflow: "auto",
+              overflow: "hidden",
               border: "2px solid #000000", //czarny - nie wiem dlaczego jak dam #black to mi po kilku importach wywala kolor
               borderRadius: "8px",
             }}
           >
-            <Worker
-              workerUrl="node_modules/pdfjs-dist/build/pdf.worker.min.js"
-            >
-              <Viewer fileUrl={URL.createObjectURL(file)} />
+            <Worker workerUrl="node_modules/pdfjs-dist/build/pdf.worker.min.js">
+              <Box display="flex" justifyContent="space-between" p={2}>
+                <ZoomOut />
+                <Box>
+                  Zoom <CurrentScale />
+                </Box>
+                <ZoomIn />
+              </Box>
+              <Viewer
+                fileUrl={URL.createObjectURL(file)}
+                plugins={[zoomPluginInstance]}
+              />
             </Worker>
           </Box>
         )}
