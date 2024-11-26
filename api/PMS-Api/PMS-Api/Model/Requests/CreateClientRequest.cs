afedb9bd2350bc00c2a12aac88f2b19aaed2c2f4
icewu <marczak70@gmail.com>
2024-11-26 22:07:49 +0100

    Client backend endpoints

---
+++ api/PMS-Api/PMS-Api/Model/Requests/CreateClientRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PMS_Api.Model.Requests;
+
+
+public class CreateClientRequest
+{
+    public string ClientName { get; set; }
+    public string ClientAddress { get; set; }
+    public string ClientEmail { get; set; }
+
+}
