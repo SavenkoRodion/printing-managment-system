41012002d07788fa2a5b7504fef0dc19c0c53e6c
RodionSavenko <savenko662@gmail.com>
2024-12-20 22:41:20 +0100

    implementing backend part

---
+++ api/PMS-Api/PMS-Api/Controllers/TestFileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMS_Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestFileController : ControllerBase
+{
+    [HttpPost]
+    public void Test(IFormFile file)
+    {
+        var lol = 1;
+        var lo1l = file;
+    }
+
+}
